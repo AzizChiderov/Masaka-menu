613c52e6018b1612c8a3aea61a95275ecd6b66dc
AzizChiderov <chiderovaziz@gmail.com>
2023-05-25 15:20:27 +0600

    add new Home

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <img className={H.column} src={Column} alt="#" />
             </div>
             <div className={H.line}>
-            <h1 className="font-bold">Добро пожаловать в Масаку!</h1>
+            <h1 className="font">Добро пожаловать в Масаку!</h1>
             </div>
         </div>
     )
