ce3ab0354f60df602e9988287ba83293b45071b8
abaikb <abaikb@icloud.com>
2023-06-20 15:19:11 +0600

    add cashing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Layout } from './components/layout';
 import { BowlPage } from './Pages/Bowls/bowl.jsx';
 import { BuisneslunchPage } from './Pages/Buisness-lunch/lunch';
@@ -20,40 +21,42 @@ import { TempuraRollsPage } from './Pages/Tempura_rolls/tempura_rolls';
 import { SetsPage } from './Pages/Sets/set';
 import { KidsMenuPage } from './Pages/Kid_menu/kid_menu';
 import { DesertPage } from './Pages/Deserts/desert';
-import { DrinksPage } from './Pages/Drinks/drink'
+import { DrinksPage } from './Pages/Drinks/drink';
 import { RamenPage } from './Pages/Ramen/ramen';
 
+const queryClient = new QueryClient();
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route path='/' element={<BuisneslunchPage />} />
-          <Route path='/buisness-lunch' element={<BuisneslunchPage />} />
-          <Route path='/bowls' element={<BowlPage />} />
-          <Route path='/tofu' element={<TofuPage />} />
-          <Route path='/pizza' element={<PizzaPage />} />
-          <Route path='/ramen' element={<RamenPage />} />
-          <Route path='/soup' element={<SoupPage />} />
-          <Route path='/salads' element={<SaladPage />} />
-          <Route path='/snacks' element={<SnacksPage />} />
-          <Route path='/chicken' element={<ChickenPage />} />
-          <Route path='/beef' element={<BeefPage />} />
-          <Route path='/fish' element={ <FishPage/> }/>
-          <Route path='/noodles' element={< NoodlesPage />}/>
-          <Route path='/wok' element={<WokPage />} />
-          <Route path='/gunkan' element={<GunkanPage />} />
-          <Route path='/rolls' element={<RollsPage />} />
-          <Route path='/baked-rolls' element={<BackedRollsPage />} />
-          <Route path='/tempura-rolls' element={<TempuraRollsPage />} />
-          <Route path='/sets' element={<SetsPage />} />
-          <Route path='/kids-menu' element={<KidsMenuPage />} />
-          <Route path='/deserts' element={<DesertPage />} />
-          <Route path='/drinks' element={<DrinksPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />} >
+            <Route path='/' element={<BuisneslunchPage />} />
+            <Route path='/buisness-lunch' element={<BuisneslunchPage />} />
+            <Route path='/bowls' element={<BowlPage />} />
+            <Route path='/tofu' element={<TofuPage />} />
+            <Route path='/pizza' element={<PizzaPage />} />
+            <Route path='/ramen' element={<RamenPage />} />
+            <Route path='/soup' element={<SoupPage />} />
+            <Route path='/salads' element={<SaladPage />} />
+            <Route path='/snacks' element={<SnacksPage />} />
+            <Route path='/chicken' element={<ChickenPage />} />
+            <Route path='/beef' element={<BeefPage />} />
+            <Route path='/fish' element={<FishPage />} />
+            <Route path='/noodles' element={<NoodlesPage />} />
+            <Route path='/wok' element={<WokPage />} />
+            <Route path='/gunkan' element={<GunkanPage />} />
+            <Route path='/rolls' element={<RollsPage />} />
+            <Route path='/baked-rolls' element={<BackedRollsPage />} />
+            <Route path='/tempura-rolls' element={<TempuraRollsPage />} />
+            <Route path='/sets' element={<SetsPage />} />
+            <Route path='/kids-menu' element={<KidsMenuPage />} />
+            <Route path='/deserts' element={<DesertPage />} />
+            <Route path='/drinks' element={<DrinksPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
