6b16545dc1f50741e3e73cb3735c8a6e5b7162b0
AzizChiderov <chiderovaziz@gmail.com>
2023-07-12 14:13:23 +0600

    kkk

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router"
 import React from 'react'
-import Header from '../Header/Header'
+import Header from '../Header/Header.jsx'
 
 const Layout = () => {
   return (
